c2d30400cc194002bab7165891935e31515a2464
Robert Pirtle <astropirtle@gmail.com>
2018-04-22 22:26:32 -0700

    add working abao tests
    note that you must use https://github.com/pirtleshell/abao fork

--- api/index.js
+++ api/index.js
@@ -2,14 +2,19 @@
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const here = filename => path.join(__dirname, filename);
 
 module.exports = posts => {
   const api = express.Router();
 
+  api.use(bodyParser.urlencoded({ extended: true }));
+  api.use(bodyParser.json());
+
   api.use('/docs', express.static(here('docs.html')));
 
+  // POSTS
   api.get('/posts', (req, res) => {
     const stuff = [
       {name: 'another post', rendered: '<h1>async magic!</h1>'},
@@ -17,6 +22,11 @@ module.exports = posts => {
 
     res.json(stuff.concat(posts));
   });
+
+  api.post('/posts', (req, res) => {
+    console.log(req.body)
+    res.json({message: 'post added'});
+  })
 
   return api
 }

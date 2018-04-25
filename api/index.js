50087a9528e385960f89de17a681215df7eddda1
Robert Pirtle <astropirtle@gmail.com>
2018-04-24 23:11:49 -0700

    add endpoint to update posts

--- api/index.js
+++ api/index.js
@@ -7,7 +7,8 @@ const bodyParser = require('body-parser');
 const here = filename => path.join(__dirname, filename);
 
 module.exports = posts => {
-  const getPostById = (req, id) => posts.filter(p => (id == p.id));
+  const getPostById = id => posts.filter(p => (id == p.id))[0];
+  const send404 = (res, message) => res.status(404).json({message});
   const api = express.Router();
 
   api.use(bodyParser.urlencoded({ extended: true }));
@@ -20,22 +21,38 @@ module.exports = posts => {
     const stuff = [
       {name: 'another post', rendered: '<h1>async magic!</h1>'},
     ];
-
     res.json(stuff.concat(posts));
   });
 
+  // Create a post
   api.post('/posts', (req, res) => {
-    console.log(req.body)
-    res.json({message: 'post added'});
+    res.json({message: 'post added (not really)'});
   });
 
+  // Retrieve a post
   api.get('/posts/:id', (req, res) => {
     const post = getPostById(req.params.id);
-    if(!post) {
-      res.status(404);
-      res.send({message: 'post not found'});
-    }
+    if(!post)
+      return send404('post not found');
     res.json(post);
+  });
+
+  // Update a post
+  api.post('/posts/:id', (req, res) => {
+    const post = getPostById(req.params.id);
+    if(!post)
+      return send404('post not found');
+    if(!post.filename)
+      return res.json({message: 'no filename found'});
+    if(req.body.text == undefined)
+      return res.json({message: 'no text given'})
+
+    // TODO: versioning
+    fs.writeFile(post.filename, req.body.text, (error, data) => {
+      if(error)
+        return res.json({error})
+      res.send({message: 'success'});
+    });
   });
 
   return api
